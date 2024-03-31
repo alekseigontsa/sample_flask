86e1da8c28a1abd21ca79ef408501db858c871be
alekseigontsa <ag354@yandex.ru>
2024-03-31 23:17:53 +0300

    add templates

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html") 
+# "<h1 style='color:blue'>Hello There!</h1>"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
